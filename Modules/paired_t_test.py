df65ba3245b8a4edad0009e917c1876aa333013c
VictorSteinrud <138033799+VictorSteinrud@users.noreply.github.com>
2024-12-15 17:09:55 +0100

    uh

---
+++ Modules/paired_t_test.py
@@ -0,0 +1,6 @@
+# The k-fold cross-validated paired t test from https://stats.stackexchange.com/questions/347256/comparing-two-models-with-statistical-testing
+
+
+# først skal vi have P_i forskel for hvert fold
+
+#Så vi skal
