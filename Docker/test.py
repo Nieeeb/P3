8b6b8cc134d9bf7324ed74202b5c3fada6bb55b5
Nieeeb <william_nieboer@hotmail.com>
2024-12-18 22:04:23 +0100

    file path

--- Docker/test.py
+++ Docker/test.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 def main():    
     url = 'https://clarity.schniebster.dk/enhance' # Url for service
-    file = {'file': open('test_00011.png', 'rb')} # Reading testing image
+    file = {'file': open('Docker/test_00011.png', 'rb')} # Reading testing image
     resp = requests.post(url=url, files=file) # Send request to service
     print(f"Response time: {resp.elapsed.total_seconds()}s") # Extract response time
     img = Image.open(io.BytesIO(resp.content)) # Decode bytearray
